22038a68dbf277fa0393232a1fa45b30c375d0da
anuragkj <f20200128@dubai.bits-pilani.ac.in>
2023-09-06 00:02:57 +0400

    Production

--- mouseless/urls.py
+++ mouseless/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from quiz import views as quiz_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', quiz_views.home, name='home'),
@@ -13,4 +15,4 @@ urlpatterns = [
     path('player/', include('player.urls')),
     path('task/', include('quiz.urls')),
     path('leaderboard/', quiz_views.leaderboard, name='leaderboard')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
