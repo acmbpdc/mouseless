82508691cd3011305d33fcb0349bbc5dbc037b85
Aakar Mathur <74296290+AakarM1@users.noreply.github.com>
2024-08-24 11:09:12 +0400

    Update urls.py
    removed register path

--- mouseless/urls.py
+++ mouseless/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', quiz_views.home, name='home'),
     path('markdownx/', include('markdownx.urls')),
     path('admin/', admin.site.urls),
-    path('register/', quiz_views.register, name='register'),
+    # path('register/', quiz_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='quiz/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='quiz/logout.html'), name='logout'),
     path('player/', include('player.urls')),
